21a7fa09d0e1d9fa8c445f45ebc21725fc978316
hisomura <hisomura.noreply@gmail.com>
2021-02-04 21:47:26 +0900

    Functionalコンポーネントに書き換え

--- src/Counter.tsx
+++ src/Counter.tsx
@@ -1,34 +1,28 @@
-import React, { Component } from "react";
+import React, { Component, useCallback, useState } from "react";
 
 type Props = { label?: string; start?: number };
 const initialState = { count: 0 };
 type CounterState = Readonly<typeof initialState>;
 
-class Counter extends Component<Props, CounterState> {
-  readonly state: CounterState = initialState;
+const Counter: React.FC<Props> = ({ label = "Counter", start }) => {
+  const [state, setState] = useState<CounterState>({ count: start ?? 0 });
 
-  componentDidMount() {
-    if (this.props.start) {
-      this.setState({ count: this.props.start });
-    }
-  }
+  const incrementCounter = useCallback(
+    (event: React.MouseEvent<HTMLSpanElement>) => {
+      const inc = event.shiftKey ? 10 : 1;
+      setState((prev) => ({ count: prev.count + inc }));
+    },
+    []
+  );
 
-  incrementCounter(event: React.MouseEvent<HTMLSpanElement>) {
-    const inc = event.shiftKey ? 10 : 1;
-    this.setState((prev) => ({ count: prev.count + inc }));
-  }
-
-  render() {
-    const { label = "Counter" } = this.props;
-    return (
-      <div>
-        <label htmlFor="counter">{label}</label>
-        <span id="counter" role="counter" onClick={this.incrementCounter.bind(this)}>
-          {this.state.count}
-        </span>
-      </div>
-    );
-  }
-}
+  return (
+    <div>
+      <label htmlFor="counter">{label}</label>
+      <span id="counter" role="counter" onClick={incrementCounter}>
+        {state.count}
+      </span>
+    </div>
+  );
+};
 
 export default Counter;
